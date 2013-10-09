c1ccbed08424b503c2e76500ad3e79b5842f4885
Mikael Gueck <gumi@iki.fi>
2013-10-09 20:48:46 +0300

    Require UTF-8 encoding in all data transfer.

--- src/main/java/mikaelhg/example/ExampleController.java
+++ src/main/java/mikaelhg/example/ExampleController.java
@@ -1,15 +1,23 @@
 package mikaelhg.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.annotation.Resource;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * This example application has only a single Spring MVC web controller,
@@ -25,8 +33,42 @@ public class ExampleController {
 
     @Resource private ExampleDao dao;
 
+    private final static ApplicationContextInitializer<ConfigurableApplicationContext> REQUIRE_UTF8 =
+            new ApplicationContextInitializer<ConfigurableApplicationContext>() {
+        @Override public void initialize(final ConfigurableApplicationContext ctx) {
+            final String encoding = System.getProperty("file.encoding");
+            if (encoding != null && !"UTF-8".equals(encoding.toUpperCase())) {
+                log.error("Your system property \"file.encoding\" is currently \"{}\". It should be \"UTF-8\". ", encoding);
+                log.error("Your environmental variable LANG is currently \"{}\". It should end \".UTF-8\". ", System.getenv("LANG"));
+                log.error("Your environmental variable LC_ALL is currently \"{}\". It should end \".UTF-8\". ", System.getenv("LC_ALL"));
+                log.error("We can't run this application properly if the Java Virtual Machine it runs in hasn't been configured to use the UTF-8 encoding. Closing down.");
+                throw new IllegalStateException("We can't run this application properly if the Java Virtual Machine it runs in hasn't been configured to use the UTF-8 encoding. Closing down.");
+            }
+        }
+    };
+
     public static void main(final String ... args) {
-        SpringApplication.run(ExampleController.class, args);
+        new SpringApplicationBuilder(ExampleController.class)
+                .initializers(REQUIRE_UTF8)
+                .web(true)
+                .run(args);
+    }
+
+    /**
+     * We want every request/response pair to be handled as UTF-8, and every response identify itself as UTF-8.
+     */
+    @Bean
+    public javax.servlet.Filter characterSetFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(final HttpServletRequest req, final HttpServletResponse res, final FilterChain chain)
+                    throws ServletException, IOException
+            {
+                req.setCharacterEncoding("UTF-8");
+                res.setCharacterEncoding("UTF-8");
+                chain.doFilter(req, res);
+            }
+        };
     }
 
     @RequestMapping(value="/")
