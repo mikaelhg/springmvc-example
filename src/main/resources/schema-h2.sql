2473e5ef559dc4ae2c382861290d4507abb9d95e
Mikael Gueck <gumi@iki.fi>
2014-01-25 18:52:41 +0200

    Add UTF-8 test data goodness.

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -4,4 +4,5 @@ CREATE MEMORY TABLE examples (
 );
 
 INSERT INTO examples (name) VALUES ('The Wire'), ('The West Wing'),
-    ('The Daily Show'), ('Rent'), ('Rango'), ('Iñtërnâtiônàlizætiøn');
+    ('The Daily Show'), ('Rent'), ('Rango'), ('Iñtërnâtiônàlizætiøn'),
+    ('TO͇̹̺ͅƝ̴ȳ̳ TH̘Ë͖́̉ ͠P̯͍̭O̚​N̐Y̡ H̸̡̪̯ͨ͊̽̅̾̎Ȩ̬̩̾͛ͪ̈́̀́͘ ̶̧̨̱̹̭̯ͧ̾ͬC̷̙̲̝͖ͭ̏ͥͮ͟Oͮ͏̮̪̝͍M̲̖͊̒ͪͩͬ̚̚͜Ȇ̴̟̟͙̞ͩ͌͝S̨̥̫͎̭ͯ̿̔̀ͅ');
