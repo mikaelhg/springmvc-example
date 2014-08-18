f9c0e24976d096923e9f0791398d3b0a5d182fd5
Mikael Gueck <gumi@iki.fi>
2014-08-18 14:03:06 +0300

    Update libraries.

--- src/main/java/mikaelhg/example/Example.java
+++ src/main/java/mikaelhg/example/Example.java
@@ -5,15 +5,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Builder;
 
 /**
  * Example domain class.
  */
 @Entity(name="examples")
-@Data @NoArgsConstructor @RequiredArgsConstructor
+@Data @NoArgsConstructor @RequiredArgsConstructor @AllArgsConstructor
+@Builder(builderClassName = "Builder")
 public class Example implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS examples (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name TEXT
+);
+
 INSERT INTO examples (name) VALUES ('The Wire'), ('The West Wing'),
     ('The Daily Show'), ('Rent'), ('Rango'), ('Iñtërnâtiônàlizætiøn'),
     ('TO͇̹̺ͅƝ̴ȳ̳ TH̘Ë͖́̉ ͠P̯͍̭O̚​N̐Y̡ H̸̡̪̯ͨ͊̽̅̾̎Ȩ̬̩̾͛ͪ̈́̀́͘ ̶̧̨̱̹̭̯ͧ̾ͬC̷̙̲̝͖ͭ̏ͥͮ͟Oͮ͏̮̪̝͍M̲̖͊̒ͪͩͬ̚̚͜Ȇ̴̟̟͙̞ͩ͌͝S̨̥̫͎̭ͯ̿̔̀ͅ');
